a367650422d3e6f3e52bd01c611d2c75316bd6ed
nasibaldirani <31454676+nasibaldirani@users.noreply.github.com>
2017-11-07 18:37:30 +0000

    changes

--- main.py
+++ main.py
@@ -3,8 +3,17 @@ import webapp2
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers["Content-Type"] = "text/plain"
-        self.response.write("Hello, it's a web app!")
+         self.response.headers["Content-Type"] = "text/html"
+         self.response.write("""
+          <html>
+            <head><title>Enter your name...</title></head>
+            <body>
+              <form action="/welcome" method="post">
+                <input type="text" name="my_name"><br>
+                <input type="submit" value="Sign In">
+              </form>
+            </body>
+            </html>""")
 
 routes = [('/', MainPage)]
 
